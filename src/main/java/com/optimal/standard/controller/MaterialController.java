4ca05cd5bbcba0dd47118fa8745d45178acbaf40
manucleal <emanuel.coitino@mercadolibre.com>
2023-07-10 16:07:26 -0300

    agregando config por defecto para cors

--- src/main/java/com/optimal/standard/controller/MaterialController.java
+++ src/main/java/com/optimal/standard/controller/MaterialController.java
@@ -8,7 +8,6 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RestController
 @Validated
-@CrossOrigin("http://localhost:3000")
 public class MaterialController {
 
   private final MaterialService materialService;
